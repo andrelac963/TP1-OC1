da31a26a96c803982b5fbb011cdf1837927f3e53
andrelac963 <andrelac963@gmail.com>
2023-10-09 21:54:44 -0300

    Update 2.s

--- 2.s
+++ 2.s
@@ -1,35 +1,49 @@
 .data
-vetor:  .word 200, 190, 340, 100 # exemplo
+vetor:  .word 200, 190, 340, 100, 900, 520, 210 # exemplo
 
-##### START MODIFIQUE AQUI START #####
-#
-# Este espaço é para você definir as suas constantes e vetores auxiliares.
-#
-##### END MODIFIQUE AQUI END #####
-      
-.text    
+.text
+la a0, vetor # endereço do vetor
+li a1, 7 # tamanho do vetor
+li a2, 200 # limiar
+li t0, 0 # contador de salários acima do limiar
+li t1, 2 # divisor para o reajuste
 
-jal ra, main 
-# Ao final do reajuste (aplica_reajuste) você deve retornar o programa para a próxima instrução abaixo, que consiste na correção/verificação.
+jal ra, main # chamando a sub-rotina main
 
-##### START INSTRUÇÃO DE CORREÇÃO/VERIFICAÇÃO #####
-# utilizado para correção (considerando um limiar de 200 para o vetor de exemplo após a aplicação do reajuste.
-
-addi a4, x0, 3 # configurando a quantidade de salários acima do limiar de 200.
+addi a4, x0, 6 # configurando a quantidade de salários acima do limiar de 200.
 beq a4, t0, FIM # Verifica a quantidade de salários acima do limiar.
 
-##### END INSTRUÇÃO DE CORREÇÃO/VERIFICAÇÃO #####
+main:
+    addi sp, sp, -4 # alocando espaço na pilha
+    sw ra, 0(sp) # salvando o endereço de retorno na pilha
 
-main: 
-##### START MODIFIQUE AQUI START #####
-    jal ra, aplica_reajuste 
-##### END MODIFIQUE AQUI END #####
+    jal ra, aplica_reajuste # chamando a sub-rotina aplica_reajuste
 
-# dica, você deve salvar o endereço da primeira chamada (em stack (sp)) para depois recuperá-lo.
+    lw ra, 0(sp) # recuperando o endereço de retorno da pilha
+    addi sp, sp, 4 # liberando espaço na pilha
+
+    jr ra
 
 aplica_reajuste:
-##### START MODIFIQUE AQUI START #####
+    lw t2, 0(a0)       # carregando o primeiro salário do vetor
 
-##### END MODIFIQUE AQUI END #####
+    div t3, t2, t1     # dividindo o salário por 2
+    add t2, t2, t3     # somando o salário com a metade do salário
+
+    sw t2, -4(a0)       # armazenando o novo salário de volta no vetor
+
+    # incrementa o contador de salários acima do limiar
+    bge t2, a2, salario_acima_limiar
+
+    addi a0, a0, 4      # incrementando endereço do vetor
+    addi a1, a1, -1     # decrementando tamanho do vetor
+
+    bnez a1, aplica_reajuste # verifica se o contador do tamanho do vetor é diferente de zero
+
+    jr ra
+
+salario_acima_limiar:
+    addi t0, t0, 1
+    jr ra
 
 FIM: addi x0, x0, 1
